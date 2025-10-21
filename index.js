3b45d2f4e08e1de04e3d1d6894d49e4eddf01087
dzakyputra45 <dzakypratama45@gmail.com>
2025-10-21 10:05:38 +0700

    Tambahkan endpoint root

--- index.js
+++ index.js
@@ -6,3 +6,11 @@ const port = 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
